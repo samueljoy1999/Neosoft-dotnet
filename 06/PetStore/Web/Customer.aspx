015251f5fc7f2a264ab4f66a80693e0405001fad
samuel <samueljoy1999@gmail.com>
2021-09-06 13:36:23 +0530

    Samuel Ramya Nivrutti

---
+++ 06/PetStore/Web/Customer.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Customer.aspx.cs" Inherits="Customer.Customer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Name" runat="server" Text="Name" Height="30px" Width="50px"></asp:Label>&nbsp;
+            <asp:TextBox ID="txtName" runat="server" OnTextChanged="txtName_TextChanged"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Email">
+            </asp:Label><asp:TextBox ID="txtEmail" runat="server" OnTextChanged="txtEmail_TextChanged"></asp:TextBox>
+        </div>  
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Zip Code">
+            </asp:Label><asp:TextBox ID="txtZip" runat="server"></asp:TextBox>
+        </div>  
+        <div>
+            <asp:Label ID="Label3" runat="server" Text="Username">
+            </asp:Label><asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+        </div>  
+        <div>
+            <asp:Label ID="Label4" runat="server" Text="Password">
+            </asp:Label><asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+        </div>  
+        <br/>
+        <div>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 06/PetStore/Web/Customer.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Data;
+using System.Configuration;
+
+namespace Customer
+{
+    public partial class Customer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string Constring = ConfigurationManager.ConnectionStrings["CustomerConnect"].ConnectionString;
+            SqlCommand command;
+            SqlConnection connection;
+            SqlParameter parameter;
+
+            
+            using (connection = new SqlConnection(Constring))
+            {
+                using (command = new SqlCommand("SP_CustomerInsert", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    parameter = command.Parameters.AddWithValue("@Name", txtName);
+                    parameter = command.Parameters.AddWithValue("@ZipCode", txtZip);
+                    parameter = command.Parameters.AddWithValue("@Email", txtEmail);
+
+                    parameter = command.Parameters.AddWithValue("@Username", txtUsername);
+                    parameter = command.Parameters.AddWithValue("@Password", txtPassword);
+                    connection.Open();
+                        
+                }
+            }
+            connection.Close();           
+
+        }
+    }
+}
